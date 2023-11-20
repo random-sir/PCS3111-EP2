19f6a04f997421a14699f8aacd3ccd0f05e05f18
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-20 18:07:11 -0300

    Update Sinal.cpp
    Implementei aqui

--- Sinal.cpp
+++ Sinal.cpp
@@ -1,31 +1,58 @@
 #include "Sinal.h"
 #include "Grafico.h"
+#include <string>
+#include <stdexcept>
 
-Sinal::Sinal(double *sequencia, int comprimento) : comprimento(comprimento)
-{
-    this->sequencia = new double[comprimento];
-    for (int i=0;i<comprimento;i++){
-        this->sequencia[i] = sequencia[i];
+using namespace std;
+
+Sinal::Sinal(double *sequencia, int comprimento){
+    if(comprimento <= 0) throw new invalid_argument("Comprimento invalido");
+    else{
+        this->comprimento = comprimento;
+        this->sequencia = new double[comprimento];
+        for (int i=0;i<comprimento;i++){
+            this->sequencia[i] = sequencia[i];
+        }
     }
 }
-Sinal::~Sinal()
-{
+
+Sinal::Sinal(double constante, int comprimento){
+    if(comprimento <= 0) throw new invalid_argument("Comprimento invalido");
+    else{
+        this->comprimento = comprimento;
+        this->sequencia = new const double[comprimento];
+        for (int i=0;i<comprimento;i++){
+            this->sequencia[i] = constante;
+        }
+    }
+}
+
+Sinal::~Sinal(){
     delete[] sequencia;
 }
 
-double *Sinal::getSequencia()
-{
+double *Sinal::getSequencia(){
     return sequencia;
 }
 
-int Sinal::getComprimento()
-{
+int Sinal::getComprimento(){
     return comprimento;
 }
 
-void Sinal::imprimir(string nomeDoSinal)
-{
+void Sinal::imprimir(string nomeDoSinal){
     Grafico* graficoSinal = new Grafico(nomeDoSinal,sequencia,comprimento);
     graficoSinal->plot();
     delete graficoSinal;
+}
+
+void Sinal::imprimir(){
+    for(int i = 0; i < comprimento; i++)
+        cout << i << "- " << sequencia[i] << endl;
+    cout << "--" << endl;
+}
+
+void Sinal::imprimir(int tamanho){
+    for(int i = 0; i < tamanho; i++)
+        cout << i << "- " << sequencia[i] << endl;
+    cout << "--" << endl;
 }
