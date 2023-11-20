5779f1c5b62702828069eeff711f1ed203c9c6a0
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-20 19:00:26 -0300

    Update Somador.h

--- Somador.h
+++ Somador.h
@@ -1,11 +1,11 @@
 #ifndef SOMADOR_H
 #define SOMADOR_H
 #include "Sinal.h"
+#include "CircuitoMISO.h"
 
-class Somador{
+class Somador: public CircuitoMISO{
 public:
     Somador();
     virtual ~Somador();
-    Sinal* processar(Sinal *sinalIN1, Sinal *sinalIN2);
 };
 #endif
