e1c79299b2330239dd86f41eb87cb764d4bf20ca
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-20 18:40:07 -0300

    Update Circuito.cpp

--- Circuito.cpp
+++ Circuito.cpp
@@ -1,1 +1,25 @@
+#include "Circuito.h"
+#include <iostream>
 
+using namespace std;
+
+int Circuito::GlobalID = 0; //atributo estático
+
+Circuito::Circuito(){
+  this->ID = ++Circuito::GlobalID;
+}
+
+Circuito::~Circuito(){
+}
+
+int Circuito::getID(){
+  return ID;
+}
+
+void Circuito::imprimir(){
+  cout << "Circuito com ID " << ID << endl;
+}
+
+int Circuito::getUltimoID(){
+  return Circuito::GlobalID;
+}
