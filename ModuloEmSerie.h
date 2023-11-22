2a5bde14b29fe1031d8690ecf32497508657e1bc
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-22 20:37:46 -0300

    Create ModuloEmSerie.h

---
+++ ModuloEmSerie.h
@@ -0,0 +1,11 @@
+#ifndef MODULOEMSERIE_H
+#define MODULOEMSERIE_H
+#include "Modulo.h"
+
+class ModuloEmSerie: public Modulo{
+  public:
+    ModuloEmSerie();
+    virtual ~ModuloEmSerie();
+};
+
+#endif
