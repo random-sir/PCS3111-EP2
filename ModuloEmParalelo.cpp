d44e3b5451ec48593e665bdad442c8355ed2d886
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-23 19:15:40 -0300

    Create ModuloEmParalelo.cpp

---
+++ ModuloEmParalelo.cpp
@@ -0,0 +1,13 @@
+#include "ModuloEmParalelo.h"
+
+ModuloEmParalelo::ModuloEmParalelo(){
+  
+}
+
+ModuloEmParalelo::~ModuloEmParalelo(){
+  
+}
+
+Sinal* ModuloEmParalelo::processar(Sinal* sinalIN){
+  
+}
