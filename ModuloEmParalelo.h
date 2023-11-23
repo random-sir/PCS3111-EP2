75c8dcf40bd6401b534c270a0ff5a871dc4ce7a2
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-23 19:11:06 -0300

    Create ModuloEmParalelo.h

---
+++ ModuloEmParalelo.h
@@ -0,0 +1,13 @@
+$ifndef MODULOEMPARALELO_H
+#define MODULOEMPARALELO_H
+#include "Modulo.h"
+
+
+class ModuloEmParalelo: public Modulo{
+  public:
+    ModuloEmParalelo();
+    virtual ~ModuloEmParalelo();
+    Sinal* processar(Sinal* sinalIN);
+};
+
+#endif
