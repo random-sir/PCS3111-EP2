8af2c83d04d9df09b2ad2e791e623822fc4b6b55
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-23 19:34:52 -0300

    Update ModuloEmParalelo.cpp

--- ModuloEmParalelo.cpp
+++ ModuloEmParalelo.cpp
@@ -1,5 +1,8 @@
 #include "ModuloEmParalelo.h"
 #include "Somador.h"
+#include <list>
+
+using namespace std;
 
 ModuloEmParalelo::ModuloEmParalelo(){
   
@@ -11,4 +14,20 @@ ModuloEmParalelo::~ModuloEmParalelo(){
 
 Sinal* ModuloEmParalelo::processar(Sinal* sinalIN){
   Somador* sum = new Somador();
+  bool temCircSISO = false; //indicador de passagem. Utilizaremos a ideia de supor que não há circSISO 
+                            //nos "circuitos" e, se ao final do laço o indicador continuar como falso ent significa
+                            //que não há nenhum circSISO
+  CircuitoSISO* circ = nullptr;
+  Sinal* sinalSomado = nullptr;
+  list<CircuitosSISO*>::iterator i;
+  for(i = circuitos->begin(); i != circuitos->end(); i++){
+    circ = dynamic_cast<CircuitoSISO*>(*i);
+    if(circ != NULL){
+      temCircSISO = true; 
+      sinalSomado = sum->processar(sinalSomado, (*i)->processar(sinalIN));
+    }
+  }
+  if(!temCircSISO) throw new logic_error("Nao ha circuitos SISO");
+  delete sum;
+  return sinalSomado;
 }
