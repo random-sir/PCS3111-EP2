77e0f60e2d4ee4771f371b34f1cb8ce1ba3805d0
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-23 19:07:02 -0300

    Update ModuloEmSerie.cpp

--- ModuloEmSerie.cpp
+++ ModuloEmSerie.cpp
@@ -1,4 +1,7 @@
 #include "ModuloEmSerie.h"
+#include <iostream>
+
+using namespace std;
 
 ModuloEmSerie::ModuloEmSerie(){
   
@@ -8,7 +11,25 @@ ModuloEmSerie::~ModuloEmSerie(){
   
 }
 
-Sinal* ModuloEmSerie::processar(){
-  
-  if()
+Sinal* ModuloEmSerie::processar(Sinal* sinalIN){
+  bool temCircSISO = false; //indicador de passagem. Utilizaremos a ideia de supor que não há circSISO 
+                                 //nos "circuitos" e, se ao final do laço o indicador continuar como falso ent significa
+                                 //que não há nenhum circSISO
+  Sinal* sinalPrinc = sinalIN;
+  CircuitoSISO* circ = nullptr;
+  list<CircuitosSISO*>::iterator i;
+  for(i = circuitos->begin(); i != circuitos->end(); i++){
+    circ = dynamic_cast<CircuitoSISO*>(*i);
+    if(circ != NULL){
+      temCircSISO = true; 
+      //evitando o overleak
+      Sinal* sinalInterm = (*i)->processar(sinalPrinc);
+      delete sinalPrinc;
+      Sinal* sinalPrinc = sinalInterm;
+      delete sinalInterm;
+    }
+  }
+}
+  if(!temCircSISO) throw logic_error("Nao ha circuitos SISO");
+  return sinalPrinc;
 }
