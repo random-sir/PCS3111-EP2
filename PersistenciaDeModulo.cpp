10695cd5ef8bb221a74bb193b3f988fb0dda6739
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-24 15:46:00 -0300

    Update PersistenciaDeModulo.cpp

--- PersistenciaDeModulo.cpp
+++ PersistenciaDeModulo.cpp
@@ -1,7 +1,8 @@
 #include "PersistenciaDeModulo.h"
 
 string tipoDeCircuitoSISO(CircuitoSISO* circ);
-string escreverModulo(Modulo* modulo);
+void escreverModulo(Modulo* modulo, ofstream arquivo);
+CircuitoSISO* tipoDeCircuitoSISO_CircSISO(string letraCirc);
 
 PersistenciaDeModulo::PersistenciaDeModulo(string nomeDoArquivo){
   this->nomeDoArquivo = nomeDoArquivo; 
@@ -11,28 +12,37 @@ PersistenciaDeModulo::~PersistenciaDeModulo(){
   
 }
 
-void PersistenciaDeModulo::salvarEmArquivo(Modulo* mod){
+void PersistenciaDeModulo::salvarEmArquivo(Modulo* mod){ //concluída, falta revisar
   ofstream output;
   ouput.open(nomeDoArquivo);
 
   if(output.fail()){
-    cout << "Arquivo nao encontrado" << endl;
+    throw new invalid_argument("Erro ao abrir o arquivo");
+    output.close();
+  }else{
+    escreverModulo(mod, ouput);
     entrada.close();
-    return
-  }
+    }
+}
 
-  escreverModulo(mod);
-          
-        }
+Modulo* PersistenciaDeModulo::lerDeArquivo(){// não concluída
+  ifstream input;
+  input.opean(nomeDoArquivo);
+
+  if(input.fail()){
+      throw new invalid_argument("Erro ao abrir o arquivo");
+      input.close();
+  }else{
+    Modulo* mod = new Modulo();
+    string letraCirc;
+    while(input){ //enquanto input.fail() nao eh false
+    input >> letraCirc;
+    mod->getCircuitos->adicionar(tipoDeCircuitoSISO_CircSISO(letraCirc));
     }
   }
 }
 
-Modulo* PersistenciaDeModulo::lerDeArquivo(){
-  
-}
-
-string tipoDeCircuitoSISO(CircuitoSISO* circ){
+string tipoDeCircuitoSISO_STR(CircuitoSISO* circ){ //bruto terminado
 //falta fazer o controle de erros corretamente
   if(dynamic_cast<Derivador*>(circ) != NULL) return "D";
   else if(dynamic_cast<Amplificador*>(circ) != NULL) return "A";
@@ -44,17 +54,41 @@ string tipoDeCircuitoSISO(CircuitoSISO* circ){
 //     throw new argument_error("Nao eh um circuito SISO");
 //     return "ERRO";
 //   }
-// }
-
-string escreverModulo(Modulo* modulo){
-  for(list<CircuitosSISO*>::iterador i = modulo->getCircuitos->begin(); i != modulo->getCircuitos->end(); i++){
-    if(tipoDeCircuitoSISO(*i) == "P" || tipoDeCircuitoSISO(*i) == "S" || tipoDeCircuitoSISO(*i) == "R"){
-      output << tipoDeCircuitoSISO(*i) << endl;
-      escreverModulo(*i);
-    }else if(tipoDeCircuitoSISO(*i) == "A") 
-      output << "A " << (*i)->getGanho << endl;
-     else 
-      output << tipoDeCircuitoSISO(*i) << endl;
-  }
-  return "f";
 }
+
+CircuitoSISO* tipoDeCircuitoSISO_CircSISO(string letraCirc){ //não terminada
+  //falta fazer o controle de erros corretamente
+  if(letraCirc[0] == "A"){ //em manutenção
+   return new Amplificador(static_cast<double>(letraCirc[]));
+  }else if(letraCirc == "D") return new Derivador();
+  else if(letraCirc == "I") return new Integrador();
+  else if(letraCirc == "P") return new ModuloEmParalelo();
+  else if(letraCirc == "S") return new ModuloEmSerie();
+  else if(letraCirc == "R") return new ModuloRealimentado();
+}
+
+void escreverModulo(Modulo* modulo, ofstream arquivo){ //concluida, falta revisar
+  for(list<CircuitosSISO*>::iterador i = modulo->getCircuitos->begin(); i != modulo->getCircuitos->end(); i++){
+    if(tipoDeCircuitoSISO_STR(*i) == "P" || tipoDeCircuitoSISO_STR(*i) == "S" || tipoDeCircuitoSISO_STR(*i) == "R"){
+      arquivo << tipoDeCircuitoSISO(*i) << endl;
+      escreverModulo(*i);
+    }else if(tipoDeCircuitoSISO_STR(*i) == "A") 
+      arquivo << "A " << (*i)->getGanho << endl;
+     else 
+      arquivo << tipoDeCircuitoSISO_STR(*i) << endl;
+  }
+  arquivo << "f" << endl;
+}
+
+Modulo* leituraModulo(Modulo* mod, ifstream arquivo){ //não terminada
+  string letraCirc;
+  while(input){ //enquanto input.fail() nao eh false
+    input >> letraCirc;
+    if(letraCirc == "P" || letraCirc == "S" || letraCirc == "R");
+      Modulo* moduloInterno = tipoDeCircuitoSISO_CircSISO(letraCirc);
+      mod->getCircuitos->adicionar(moduloInterno);
+      while(letraCirc != "f"){
+       moduloInterno->getCircuitos->adicionar(tipoDeCircuitoSISO_CircSISO(letraCirc));
+      }
+}
+
