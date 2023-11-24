84e16318bb77adf2f8a548517266f69b2ec52cc3
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-24 10:37:06 -0300

    Update ModuloEmSerie.h

--- ModuloEmSerie.h
+++ ModuloEmSerie.h
@@ -6,7 +6,7 @@ class ModuloEmSerie: public Modulo{
   public:
     ModuloEmSerie();
     virtual ~ModuloEmSerie();
-    Sinal* processar(*sinalIN);
+    Sinal* processar(Sinal *sinalIN);
 };
 
 #endif
