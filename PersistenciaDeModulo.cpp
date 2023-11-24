998d0b51d764cda75028e8173524deac4264b41d
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-24 13:01:06 -0300

    Update PersistenciaDeModulo.cpp

--- PersistenciaDeModulo.cpp
+++ PersistenciaDeModulo.cpp
@@ -1,5 +1,8 @@
 #include "PersistenciaDeModulo.h"
 
+string tipoDeCircuitoSISO(CircuitoSISO* circ);
+string escreverModulo(Modulo* modulo);
+
 PersistenciaDeModulo::PersistenciaDeModulo(string nomeDoArquivo){
   this->nomeDoArquivo = nomeDoArquivo; 
 }
@@ -18,15 +21,40 @@ void PersistenciaDeModulo::salvarEmArquivo(Modulo* mod){
     return
   }
 
-  CircuitoSISO* circ = nullptr;
-  for(list<CircuitosSISO*>::iterador i = mod->getCircuitos->begin(); i != mod->getCircuitos->end(); i++){
-    circ = dynamic_cast<c>(*i);
-    if(dynamic_cast<c>(*i))
-    
+  escreverModulo(mod);
+          
+        }
+    }
   }
-  
 }
 
 Modulo* PersistenciaDeModulo::lerDeArquivo(){
   
+}
+
+string tipoDeCircuitoSISO(CircuitoSISO* circ){
+//falta fazer o controle de erros corretamente
+  if(dynamic_cast<Derivador*>(circ) != NULL) return "D";
+  else if(dynamic_cast<Amplificador*>(circ) != NULL) return "A";
+  else if(dynamic_cast<Integrador*>(circ) != NULL) return "I";
+  else if(dynamic_cast<ModuloEmParalelo*>(circ) != NULL) return "P";
+  else if(dynamic_cast<ModuloEmSerie*>(circ) != NULL) return "S";
+  else if(dynamic_cast<ModuloRealimentado*>(circ) != NULL) return "R";
+//   else{
+//     throw new argument_error("Nao eh um circuito SISO");
+//     return "ERRO";
+//   }
+// }
+
+string escreverModulo(Modulo* modulo){
+  for(list<CircuitosSISO*>::iterador i = modulo->getCircuitos->begin(); i != modulo->getCircuitos->end(); i++){
+    if(tipoDeCircuitoSISO(*i) == "P" || tipoDeCircuitoSISO(*i) == "S" || tipoDeCircuitoSISO(*i) == "R"){
+      output << tipoDeCircuitoSISO(*i) << endl;
+      escreverModulo(*i);
+    }else if(tipoDeCircuitoSISO(*i) == "A") 
+      output << "A " << (*i)->getGanho << endl;
+     else 
+      output << tipoDeCircuitoSISO(*i) << endl;
+  }
+  return "f";
 }
