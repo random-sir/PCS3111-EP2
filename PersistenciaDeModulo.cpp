f25a843012c9ad1d76ded72941e84b5facc04477
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-24 11:49:22 -0300

    Create PersistenciaDeModulo.cpp

---
+++ PersistenciaDeModulo.cpp
@@ -0,0 +1,17 @@
+#include "PersistenciaDeModulo.h"
+
+PersistenciaDeModulo(string nomeDoArquivo){
+  this->nomeDoArquivo = nomeDoArquivo; 
+}
+
+virtual ~PersistenciaDeModulo(){
+  
+}
+
+void salvarEmArquivo(Modulo* mod){
+  
+}
+
+Modulo* lerDeArquivo(){
+  
+}
