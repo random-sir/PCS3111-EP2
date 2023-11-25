f5ff9adf61bae4087baff4405d35d850b19320f7
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-11-24 23:44:21 -0300

    Update ModuloEmParalelo.h

--- ModuloEmParalelo.h
+++ ModuloEmParalelo.h
@@ -1,6 +1,12 @@
 #ifndef MODULOEMPARALELO_H
 #define MODULOEMPARALELO_H
+#include "CircuitoSISO.h"
 #include "Modulo.h"
+#include "Somador.h"
+#include <iostream>
+#include <list>
+
+using namespace std;
 
 class ModuloEmParalelo: public Modulo{
   public:
