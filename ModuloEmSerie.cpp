a72c0b8ebd15604897176dd6721e905d13b17695
random-sir <antoni.torres@outlook.com>
2023-11-29 10:06:19 -0300

    Memory leak em ModuloEmSerie consertado, verifique quando conseguir pedro.

--- ModuloEmSerie.cpp
+++ ModuloEmSerie.cpp
@@ -11,14 +11,20 @@ Sinal* ModuloEmSerie::processar(Sinal* sinalIN){
                             //nos "circuitos" e, se ao final do laço o indicador continuar como falso 
                             //ent significa que não há nenhum circSISO
   CircuitoSISO* circ = nullptr;
-  Sinal* sinalPrinc = sinalIN;
+  Sinal* sinalPrinc1 = sinalIN;
+  Sinal* sinalPrinc2 = sinalPrinc1; //caso nao ocorra nenhum processamento o sinal retornado sera sinalIN
   for(list<CircuitoSISO*>::iterator i = circuitos->begin(); i != circuitos->end(); i++){
     circ = dynamic_cast<CircuitoSISO*>(*i);
     if(circ != NULL){
       temCircSISO = true; 
-      sinalPrinc = (*i)->processar(sinalPrinc);
+      sinalPrinc2 = (*i)->processar(sinalPrinc1);     //deve ser feito dessa forma para evitar um memory leak
+
+      if (sinalPrinc1 != sinalIN) delete sinalPrinc1;//devemos deletar a memoria alocada em sinalPrinc1 (desde que não seja sinalIN),
+                                                    //pois o metodo processar não deleta.
+      sinalPrinc1 = sinalPrinc2;                    
+      
     }
   }
   if(!temCircSISO) throw new logic_error("Nao ha circuitos SISO");
-  return sinalPrinc;
+  return sinalPrinc2;
 }
