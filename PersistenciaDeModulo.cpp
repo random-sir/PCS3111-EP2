ec893d43982efa55e1d6e9c84b7fd76fca063bf6
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-12-08 13:13:28 -0300

    Update PersistenciaDeModulo.cpp

--- PersistenciaDeModulo.cpp
+++ PersistenciaDeModulo.cpp
@@ -20,60 +20,49 @@ CircuitoSISO *tipoDeCircuitoSISO_CircSISO(string letraCirc, ifstream &arquivo);
 void escreverModulo(Modulo *modulo, ofstream &arquivo);
 void leituraModulo(Modulo *modulo, ifstream &arquivo);
 
-PersistenciaDeModulo::PersistenciaDeModulo(string nomeDoArquivo)
-{
+PersistenciaDeModulo::PersistenciaDeModulo(string nomeDoArquivo){
   this->nomeDoArquivo = nomeDoArquivo;
 }
 
-PersistenciaDeModulo::~PersistenciaDeModulo()
-{
+PersistenciaDeModulo::~PersistenciaDeModulo(){
 }
 
-void PersistenciaDeModulo::salvarEmArquivo(Modulo *mod)
-{ // falta revisar
+void PersistenciaDeModulo::salvarEmArquivo(Modulo *mod){ 
   ofstream output;
   output.open(nomeDoArquivo);
 
-  if (output.fail())
-  {
+  if (output.fail()){
     throw new invalid_argument("Erro ao abrir o arquivo");
     output.close();
   }
-  else
-  {
-    escreverModulo(mod, output); // função recursiva
+  else{
+    escreverModulo(mod, output); //função recursiva
     output.close();
   }
 }
 
-Modulo *PersistenciaDeModulo::lerDeArquivo()
-{ // falta revisar
+Modulo *PersistenciaDeModulo::lerDeArquivo(){
   ifstream input;
   input.open(nomeDoArquivo);
 
-  if (input.fail())
-  {
+  if (input.fail()){
     throw new invalid_argument("Erro ao abrir o arquivo");
     input.close();
     return nullptr;
-  }
-  else
-  {
+  }else{
     string letraModulo;
     input >> letraModulo;
     if (letraModulo != "P" && letraModulo != "S" && letraModulo != "R")
       throw new logic_error("Arquivo com formatacao inesperada");
     Modulo *mod = static_cast<Modulo *>(tipoDeCircuitoSISO_CircSISO(letraModulo, input));
-    leituraModulo(mod, input); // função recursiva
+    leituraModulo(mod, input); //função recursiva
     input.close();
     return mod;
   }
 }
 
-string tipoDeCircuitoSISO_STR(CircuitoSISO *circ)
-{ // falta revisar
+string tipoDeCircuitoSISO_STR(CircuitoSISO *circ){ 
   // recebe um circuito e retorna a letra correspondente segundo a descrição do enunciado do EP
-  // Para descobrir qual era o tipo do circuito utilizei o cast dinâmico
   if (dynamic_cast<Derivador *>(circ) != NULL)
     return "D";
   if (dynamic_cast<Amplificador *>(circ) != NULL)
@@ -90,10 +79,8 @@ string tipoDeCircuitoSISO_STR(CircuitoSISO *circ)
   return "ERRO";
 }
 
-CircuitoSISO *tipoDeCircuitoSISO_CircSISO(string letraCirc, ifstream &arquivo)
-{ // falta revisar
+CircuitoSISO *tipoDeCircuitoSISO_CircSISO(string letraCirc, ifstream &arquivo){ 
   // recebe uma letra e um arquivo e cria um circuito correspondente à letra
-  // No caso do amplificador, faço uma leitura a mais do arquivo para coletar o ganho
   if (letraCirc == "A")
   {
     double ganho;
@@ -114,31 +101,28 @@ CircuitoSISO *tipoDeCircuitoSISO_CircSISO(string letraCirc, ifstream &arquivo)
   return nullptr;
 }
 
-void escreverModulo(Modulo *modulo, ofstream &arquivo)
-{ // concluida, falta revisar
-  // basicamente pega os circuitos da lista de circuitos interna do módulo e acha a correspondente letra utilizando a função escreverModulo
+void escreverModulo(Modulo *modulo, ofstream &arquivo){ 
+  // pega os circuitos da lista de circuitos interna do módulo e acha a correspondente letra utilizando a função escreverModulo
   arquivo << tipoDeCircuitoSISO_STR(modulo) << endl;
   for (list<CircuitoSISO *>::iterator i = modulo->getCircuitos()->begin(); i != modulo->getCircuitos()->end(); i++)
   {
-    string tipoDoCircuitoAtual = tipoDeCircuitoSISO_STR(*i); // evitar chamar essa funcao desnecessariamente
-    arquivo << tipoDoCircuitoAtual;                          // imprimir tipo do circuito
+    string tipoDoCircuitoAtual = tipoDeCircuitoSISO_STR(*i); 
+    arquivo << tipoDoCircuitoAtual;                         
     if (tipoDoCircuitoAtual == "A")
       arquivo << " " << (static_cast<Amplificador *>(*i))->getGanho(); // caso especial do ganho
     arquivo << endl;
     if (tipoDoCircuitoAtual == "P" || tipoDoCircuitoAtual == "S" || tipoDoCircuitoAtual == "R")
-      escreverModulo(static_cast<Modulo *>(*i), arquivo); // recursividade para imprimir //os membros de modulos internos
+      escreverModulo(static_cast<Modulo *>(*i), arquivo);
   }
   arquivo << "f" << endl;
 }
 
-void leituraModulo(Modulo *modulo, ifstream &arquivo)
-{ // terminada
+void leituraModulo(Modulo *modulo, ifstream &arquivo){ 
   // cria o módulo descrito no arquivo
   string letraCirc;
   Modulo *moduloInterno = nullptr;
   bool ehModulo;
-  while (arquivo)
-  { // enquanto arquivo.fail() nao eh false
+  while (arquivo){
     arquivo >> letraCirc;
     if (letraCirc == "f")
       return;
